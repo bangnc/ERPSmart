861fea6b0c6de50ad67a6f56783ac45d5007f881
BANGNC\BangNC <bangnc85@gmail.com>
2020-12-21 11:54:09 +0700

    cmd

---
+++ ERP_SMART.API/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+using System;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ERP_SMART.API.Controllers
+{
+    public class BaseController: ApiController
+    {
+        /// <summary>
+        /// get Id Current bangnc quang teo eeee
+        /// </summary>
+        /// <returns></returns>
+      public Guid GetUserId()
+        {
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {               
+                var identity = (ClaimsIdentity)User.Identity;
+                IEnumerable<Claim> claims = identity.Claims;
+                var claimId = claims.FirstOrDefault(x => x.Type == "id");
+                return Guid.Parse(claimId.Value);
+
+            }
+            return Guid.Empty;
+        }
+    }
+}
